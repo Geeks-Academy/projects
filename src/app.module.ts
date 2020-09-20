00ba7dd89a7aea8eab7383d27e98718eec8efd5c
cisowianka62 <miloszcisowski97@gmail.com>
2020-09-21 01:38:40 +0200

    added module to connect with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(),MongooseModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: async (config: ConfigService) => ({
+      uri: config.get('MONGODB_URL'),
+      useNewUrlParser: true
+    }),
+    inject: [ConfigService],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
