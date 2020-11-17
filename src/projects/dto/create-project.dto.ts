42fa8d36099bccc6b5d9c8028799d0b26bfc6990
cisowianka62 <miloszcisowski97@gmail.com>
2020-11-17 20:48:18 +0100

    add fields to dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,8 @@
-export class CreateProjectDto {}
+export class CreateProjectDto {
+  readonly name: string;
+  readonly description: string;
+  readonly url: string;
+  readonly isLookingForContributors: boolean;
+  readonly creator: string;
+  readonly contributors: string[];
+}
